a678a11e61772c575c2934abfd9244d5a33f10cc
Ryan <castaneda.ry@gmail.com>
2016-09-28 23:24:22 +0800

    added immutable testing

--- article.jsx
+++ article.jsx
@@ -1,27 +1,17 @@
 var Article = React.createClass({
-    getInitialState: () => {
-        return {open: true};
-    },
-    toggle: function() {
-        this.setState({open: !this.state.open});
-    },
     render: function() {
         console.log("rendering article", this.props);
+        var article = this.props;
+
         let content = (
             <div>
-                <div>{this.props.content}</div>
-                <button onClick={this.toggle}>HIDE</button>
+                <div>{article.open && article.content || 'THIS ARTICLE IS HIDDEN'}</div>
+                <button onClick={(e)=>{
+                    this.props.onToggle(article)
+                }}>{article.open && 'HIDE' || 'SHOW'}</button>
             </div>
         );
 
-        if (!this.state.open) {
-            content = (
-                <div>
-                    <p>Article HIDDEN</p>
-                    <button onClick={this.toggle}>SHOW</button>
-                </div>
-            )
-        }
 
         return (
             <div>
--- articleList.jsx
+++ articleList.jsx
@@ -2,25 +2,63 @@ var ArticleList = React.createClass({
     getInitialState: function() {
         return {
             articles: [{
+                id: 1,
                 content: 'First Article',
+                open: true
             }, {
-                content: 'Second Article'
-            }]
+                id: 2,
+                content: 'Second Article',
+                open: true
+            }],
+            allShown: true
         };
     },
     toggleAll: function () {
+        let articles = this.state.articles.map((article) => {
+            article.open = !article.open;
+            return article;
+        });
+
+        console.log("articles", articles);
+
+        this.setState({
+            allShown: !this.state.allShown,
+            articles: articles,
+        });
+    },
+    toggle: function (article) {
+        article.open = !article.open;
+        // this.forceUpdate();
+
+        // Second try
+        var idx = this.state.articles.findIndex(function(element, index) {
+            return element.id === article.id;
+        });
+
+        // this.state.articles[idx] = article;
+
+        // this.setState({
+        //     articles: this.state.articles
+        // })
+        var map = Immutable.fromJS(this.state.articles);
+        //IMMUTABLE SOLUTION
+        this.setState({
+            articles: map.set(idx, article)
+        })
 
     },
     render: function() {
-        console.log("rendering articleLList");
         let articles = this.state.articles.map((article) => {
             return (
-                <Article content={article.content}></Article>
+                <Article {...article} onToggle={this.toggle}></Article>
             );
         });
 
         return (
             <div>
+                <button onClick={this.toggleAll}>
+                {this.state.allShown ? 'Hide' : 'Show'}
+                </button>
                 {articles}
             </div>
         );
