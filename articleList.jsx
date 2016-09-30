c0575b280242b8d3beb75707bb395b68c9df0f33
Ryan <ryan@plantminer.com.au>
2016-09-30 11:36:26 +0800

    iupdate

--- articleList.jsx
+++ articleList.jsx
@@ -2,14 +2,16 @@ var ArticleList = React.createClass({
     getInitialState: function() {
         return {
             articles: [{
+                id: 1,
                 content: 'First Article',
             }, {
+                id: 2,
                 content: 'Second Article'
             }]
         };
     },
     render: function() {
-        console.log("rendering articleLList");
+        console.log("rendering articleList", this.state.articles);
         let articles = this.state.articles.map((article) => {
             return (
                 <Article content={article.content}></Article>
